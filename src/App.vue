7343d581592d88c1fa869347de326ab939d566bd
Alex Ramsdell <alexramsdell@gmail.com>
2018-02-15 19:32:33 -0800

    move index.html styles into app component.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
+<style>
+    html,
+    body,
+    #app {
+        height: 100%;
+        width: 100%;
+    }
+    body, 
+    body * {
+        box-sizing: border-box;
+        font-family: "Comfortaa", cursive;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+    }
+    a, div {
+        -webkit-tap-highlight-color: rgba(0,0,0,0);
+        -webkit-tap-highlight-color: transparent;
+    }
+    a, .router-link-active {
+        text-decoration: none;
+    }
+</style>
 <template>
     <div id="app" class="theme" :class="themeClass">
-        <modal></modal>
         <router-view></router-view>
     </div>
 </template>
