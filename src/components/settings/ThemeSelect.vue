ac865f075e75293ef6f4836b5ee1053492e918e1
Alex Ramsdell <alexramsdell@gmail.com>
2018-03-26 20:12:43 -0700

    wip. colorgrid component

--- src/components/settings/ThemeSelect.vue
+++ src/components/settings/ThemeSelect.vue
@@ -4,7 +4,6 @@
     @import '../../../style/themes.scss'; 
 
     .theme-select-container {
-        background: white;
         height: 100%; 
         width: 100%;
         align-content: center;
@@ -14,46 +13,25 @@
         -webkit-flex-flow: row wrap;
         justify-content: space-around;
 
-        .color-theme {
-            text-align: center;
-            display: inline-block;
-            align-content: center;
-            justify-content: center;
-        }
-        .color-row {
-            display: inline-block;
-            display: block;
-            margin: 0 auto;
-            padding: 0;
-        }
-        .color {
-            padding: 0;
-            margin: 0 auto;
-            display: inline-block;
-            height: 50%;
-            width: 50%;
-        }
         ul, li, h1 {
             margin: 0;
             padding: 0;
         } 
 
+        .color-grid {
+            
+        }
+
         @media(max-width: 750px) {
-            .color-row {
+            .color-grid {
                 height: 80px;
                 width: 80px;
             }
         }
         @media(min-width: 751px) {
-            .color-row {
-                height:160px;
+            .color-grid {
+                height: 160px;
                 width: 160px;
-            }
-        }
-        @media(min-width: 400px) {
-            .color-row {
-                height: 60px;
-                width: 60px;
             }
         }
     }
@@ -61,22 +39,19 @@
 
 <template>
     <ul class="theme-select-container">
-        <ul class="color-theme" @click="selectTheme(index)" v-for="(themes, themeName) in appThemes">
-            <li>
-                <h3>{{themeName}}</h3>
-            </li>
-            <ul class="color-row">
-                <li 
-                    v-for="color in themes.slice(0,2)"
-                    :style="{background: color}" 
-                    class="color">
-                </li>
-                <li 
-                    v-for="color in themes.slice(2)"
-                    :style="{background: color}" 
-                    class="color">
-                </li>
-            </ul>
+        <ul 
+            v-for="(colors, name, index) in appThemes"
+            @click="selectTheme(name)" 
+            :class="{ 'current-theme': isCurrentTheme(name)}"
+            class="color-theme">
+
+            <li><h3>{{name}}</h3></li>
+
+            <app-colorgrid 
+                class="color-grid"
+                :num-rows="2" 
+                :colors="colors" 
+                :selected="isCurrentTheme(name)" />
         </ul>
     </ul>
 </template>
@@ -85,20 +60,32 @@
     import store from '../../store';
     import styles from '../../../style/themes.scss'; 
     import utils from 'Common/utils';
-
+    import ColorGrid from './ColorGrid';
 
     export default {
         name: 'ei-theme-select',
+        components: {
+            'app-colorgrid': ColorGrid,
+        },
         data: function() {
             return {
-                appThemes: utils.cssToThemesObj(styles)
+                appThemes: utils.cssToThemesObj(styles),
             };
         },
         methods: {
-            selectTheme(index) {
-                //console.log('updateThemes', this.appThemes[index].name);
+            selectTheme(newTheme) {
+                store.commit('updateTheme', newTheme);
+            },
+            isCurrentTheme(name) {
+                console.log(name);
+                return name == store.getters.theme;
             }
-        }
+        },
+        computed: {
+            currentTheme() {
+                return store.getters.theme;
+            }
 
+        }
     };
 </script>
